f994d9b9be5a364b70e181aba75e499a18001435
sarthakjain7432 <69512822+sarthakjain7432@users.noreply.github.com>
2023-11-30 16:09:34 +0530

    -

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg"
+import { links,social } from "./data";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [showLinks, setShowLinks] = useState(true);
+  const toggle = ()=>{
+    setShowLinks(!showLinks);
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="main">
+      <div>
+        <img src={logo} alt="" style={{width:"220px",marginLeft:"5px"}}/>
+        <span className="toggle" onClick={toggle} style={{"float":"right", "marginRight":"10px"}}>|||</span>
+      </div>
+        <div style={{marginTop:"10px"}}>
+          <div className={showLinks ? "links show" : "links"}>
+          { links.map((item)=>  
+              <a style={{"marginRight":"20px","textDecoration":"none"}} href={item.url}>{item.text}</a> 
+            )
+          }      
+          </div>
+        </div>
+        <div className="socials">
+          {
+            social.map((item)=>
+              <a style={{marginRight:"10px","color":"blue"}} href={item.url}>{item.icon}</a>           
+            )
+          }
+        </div>          
+      </div>
+    </>
   );
 }
 
